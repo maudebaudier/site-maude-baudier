067398dd44beea4ccc57d3927faa6acd4d2eb14e
maudebaudier <maude.baudier@smatchy.app>
2026-06-25 18:24:07 +0200

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build
 export default defineConfig({
-  site: 'https://www.maudebaudier.com',
+  site: 'https://maudebaudier.com',
   // Le site est 100% statique : Astro génère du HTML pur dans /dist
   build: {
     format: 'directory',
